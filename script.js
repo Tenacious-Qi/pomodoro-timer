cd51f1722e1fdd4d2b4b99754143a9b074acf6d4
Tenacious-Qi <joe.mccann@mailbox.org>
2020-04-06 19:00:05 -0400

    added audio and ability to change break times

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let seconds = 0
 let timerRunning = false;
 let paused = false;
 let interval;
+let timerEndInterval;
 let pausedTime;
 let initialTime;
-
+let shortBreakMinutes;
+let longBreakMinutes;
 
 // -- QUERY SELECTORS --  //
+const audio = document.querySelector('#alarm');
 const timerDisplay = document.querySelector('.time-display')
 const tomato = document.querySelector('.btn-tomato');
 const start = document.querySelector('.btn-start');
@@ -15,21 +18,30 @@ const stop = document.querySelector('.btn-stop');
 const reset = document.querySelector('.btn-reset');
 const shortBreak = document.querySelector('.btn-break-short');
 const longBreak = document.querySelector('.btn-break-long');
+
+// query selectors for changing timer settings
 const tomatoSlider = document.querySelector('.tomato-slider');
+const increaseShortBreakTime = document.querySelector('.btn-short-break-increase');
+const decreaseShortBreakTime = document.querySelector('.btn-short-break-decrease')
+const increaseLongBreakTime = document.querySelector('.btn-long-break-increase');
+const decreaseLongBreakTime = document.querySelector('.btn-long-break-decrease');
 let inputValue = document.querySelector('.input-value');
+let shortBreakValue = document.querySelector('.short-break-value');
+let longBreakValue = document.querySelector('.long-break-value');
+
 
 timerDisplay.textContent = minutes + `:0${seconds}`
 initialTime = timerDisplay.textContent.split(':');
 
 // -- EVENT LISTENERS -- //
 shortBreak.addEventListener('click', function() {
-  minutes = 5;
+  minutes = parseInt(shortBreakValue.textContent);
   takeBreak();
   padMinutesAndSeconds();
 });
 
 longBreak.addEventListener('click', function(){
-  minutes = 10;
+  minutes = parseInt(longBreakValue.textContent);
   takeBreak();
   padMinutesAndSeconds();
 });
@@ -46,9 +58,8 @@ stop.addEventListener('click', function() {
   clearInterval(interval);
   timerRunning = false;
   paused = true;
-  //get time at moment timer is stopped. split into array. arr[0] = minutes, arr[1] = seconds
+  //get time at moment timer is stopped.
   pausedTime = timerDisplay.textContent.split(':')
-  
 });
 
 start.addEventListener('click', function() {
@@ -62,7 +73,29 @@ reset.addEventListener('click', resetTimer);
 
 tomatoSlider.addEventListener('change', updateTimer);
 tomatoSlider.addEventListener('mousemove', function() {
-  inputValue.textContent = `${this.value}`;
+  inputValue.textContent = `${this.value} min`;
+});
+
+// EVENT LISTENERS TO HANDLE CHANGES TO BREAK DURATION. 
+// add or subtract a minute depending on which button is clicked
+increaseShortBreakTime.addEventListener('click', function() {
+  shortBreakValue.textContent = parseInt(shortBreakValue.textContent) + 1;
+});
+
+decreaseShortBreakTime.addEventListener('click', function() {
+  if (parseInt(shortBreakValue.textContent) > 1) {
+    shortBreakValue.textContent = parseInt(shortBreakValue.textContent) - 1;
+  }
+});
+
+increaseLongBreakTime.addEventListener('click', function() {
+  longBreakValue.textContent = parseInt(longBreakValue.textContent) + 1;
+});
+
+decreaseLongBreakTime.addEventListener('click', function() {
+  if (parseInt(longBreakValue.textContent) > 1) {
+    longBreakValue.textContent = parseInt(longBreakValue.textContent) - 1;
+  }
 });
 
 // -- FUNCTIONS -- //
@@ -76,17 +109,17 @@ function updateTimer() {
   pausedTime[1] = 0
   seconds = 0;
   timerDisplay.textContent = this.value + `:0${seconds}`
-  clearInterval(interval);
   timerRunning = false;
+  clearInterval(interval);
   padMinutesAndSeconds();
 }
 
 function takeBreak() {
   seconds = 0;
   paused = false;
+  timerRunning = true;
   clearInterval(interval);
   timerDisplay.textContent = minutes + `:0${seconds}`
-  timerRunning = true;
   timer();
 }
 
@@ -97,6 +130,8 @@ function resetTimer() {
   minutes = parseInt(initialTime[0]);
   seconds = parseInt(initialTime[1]);
   timerDisplay.textContent = minutes + `:0${seconds}`
+  timerDisplay.style.color = "#dfe6e9"
+  padMinutesAndSeconds();
 }
 
 // pad minutes or seconds with zeros accordingly
@@ -118,7 +153,7 @@ function padMinutesAndSeconds() {
 function timer() {
   minutes = minutes - 1;
   seconds = 59;
-    // if `stop` is clicked, then start timer where it left off
+    // if `STOP` is clicked, then start timer where it left off
     if (paused === true) {
       minutes = parseInt(pausedTime[0]);
       seconds = parseInt(pausedTime[1]);
@@ -127,17 +162,24 @@ function timer() {
   //setInterval Function
   interval = setInterval(function() {
   timerDisplay.textContent = minutes + `:${seconds}`;
-
+    
     padMinutesAndSeconds();
-      
     seconds = seconds - 1;
-    if (seconds < 0) {
+    if (seconds <= 0) {
       seconds = seconds + 60
       minutes = minutes - 1
     }
+
     if (minutes < 0) {
       clearInterval(interval)
-      alert('Time is up!')
+      audio.play();
+      timerDisplay.textContent = "Time\'s up!";
+      timerDisplay.style.color = "#2d3436"
+      //reset timer after displaying "Time's up!"
+      timerEndInterval = setInterval(function() {
+        resetTimer();
+        clearInterval(timerEndInterval);
+      }, 10000);
     }
   }, 1000);
 }
